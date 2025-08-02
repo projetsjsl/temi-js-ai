009735c1769c58b1e8254f32fae31951fca89cc2
projetsjsl <projetsjsl@gmail.com>
2025-08-02 07:07:23 -0400

    Add files via upload

---
+++ chart.js
@@ -0,0 +1,8 @@
+
+/*!
+ * Chart.js v4.4.0 - Basic UMD Build (Minified)
+ * Source: https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.js
+ */
+!function(t,e){"object"==typeof exports&&"undefined"!=typeof module?e(exports,require("chart.js")):"function"==typeof define&&define.amd?define(["exports","chart.js"],e):e((t="undefined"!=typeof globalThis?globalThis:t||self).Chart={},t.Chart)}(this,(function(t,e){"use strict";
+/* chart.js minified core content would continue here (omitted due to size limits) */
+t.Chart=e;Object.defineProperty(t,"__esModule",{value:!0})}));
