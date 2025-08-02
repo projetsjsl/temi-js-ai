50e25405ed32b95bcfa65564be49737d2b769854
projetsjsl <projetsjsl@gmail.com>
2025-08-02 07:10:49 -0400

    Update chart.js

--- chart.js
+++ chart.js
@@ -6,3 +6,4 @@
 !function(t,e){"object"==typeof exports&&"undefined"!=typeof module?e(exports,require("chart.js")):"function"==typeof define&&define.amd?define(["exports","chart.js"],e):e((t="undefined"!=typeof globalThis?globalThis:t||self).Chart={},t.Chart)}(this,(function(t,e){"use strict";
 /* chart.js minified core content would continue here (omitted due to size limits) */
 t.Chart=e;Object.defineProperty(t,"__esModule",{value:!0})}));
+
